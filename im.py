056291d7fb08266118dacc67144f971a9a9e37bf
okraj <hlavsa.jara@seznam.cz>
2021-12-22 18:16:43 +0100

    changes

--- im.py
+++ im.py
@@ -1,11 +1,10 @@
 from bs4 import BeautifulSoup
-import requests, json, openpyxl
-import urllib
-
 from random import randint
 from time import sleep
-
 from openpyxl import load_workbook
+
+import requests, json, openpyxl, urllib
+import pandas as pd
 
 headers = {
         'Access-Control-Allow-Origin': '*',
@@ -15,17 +14,37 @@ headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
     }
 
+# Load excel, delete NaNs, save excel
+df = pd.read_excel('335319.xlsx') 
+df.dropna(inplace=True)
+df.to_excel('prefinal.xlsx')
+
+# load excel, delete rows n cols, save excel
+wb = load_workbook('prefinal.xlsx')
+ws = wb.active
+ws.delete_cols(5, 1)
+ws.delete_cols(1, 2)
+ws.delete_rows(1,2)
+wb.save('final.xlsx')
+
+# convert excel to txt
+with open('test.txt', 'w') as file:
+    pd.read_excel('final.xlsx').to_string(file, index=False)
+
+
+# create final excel template
 excel = openpyxl.Workbook()
 sheet = excel.active
-sheet.title = 'antik'
+sheet.title = 'seznam antiku'
 sheet.append(['název', 'autor', 'odkaz', 'cena', 'antik'])
 
+# main starts here
 huge_list = []
-
-with open('file.txt', "r") as f:
+with open('test.txt', "r") as f:
   for line in f:
-    hg = line.replace('++','').replace('(pseudonym)', '').replace('. ', '').replace('\t', ' ').replace('kolektiv autorů','').replace('  ', '')
+    hg = line.replace('++','').replace('(pseudonym)', '').replace('. ', '').replace('\t', ' ').replace('kolektiv autorů','')
     huge_list.append(hg.strip())
+
 
 def get_organic_results():
     for query in huge_list:
@@ -67,6 +86,7 @@ def get_organic_results():
       data.append([title, author, link, price, ant_name])
       sheet.append([title, author, link, price, ant_name])
       
+      # pro vic vysledku na jeden dotaz use this:
       # for item in soup.select('.my-md-row'):
       #     title = item.find('div', {'class': 'my-md-td searchList__product__info'}).find('h2').text
       #     link = item.find('a', {'class': 'btn searchList__product__vendor__bottom__link'})['href']
@@ -84,8 +104,7 @@ def get_organic_results():
       print(data)
 
 get_organic_results()
-
-excel.save('antik.xlsx')
+excel.save('seznam_knih.xlsx')
 
 
 
